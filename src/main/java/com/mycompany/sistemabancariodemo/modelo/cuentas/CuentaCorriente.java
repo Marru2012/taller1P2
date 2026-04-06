da1ed54c8fff91bb39abf9a1bcd60df64f9b131e
unknown <emezielover@gmail.com>
2026-04-06 00:32:28 -0500

    creacion de cuentaCredito

---
+++ src/main/java/com/mycompany/sistemabancariodemo/modelo/cuentas/CuentaCorriente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemabancariodemo.modelo.cuentas;
+
+/**
+ *
+ * @author LUIS MIGUEL
+ */
+public class CuentaCorriente {
+    
+}
