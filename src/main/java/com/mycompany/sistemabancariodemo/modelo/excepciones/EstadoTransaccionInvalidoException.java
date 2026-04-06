d3785a9b2d2b348b54558dbdc0835196a9ecefdf
Jorge Luis Gomez <gomezlopezmarru2023@gmail.com>
2026-04-06 02:42:51 -0500

    correcciones a banco y se agrega transaccion

--- src/main/java/com/mycompany/sistemabancariodemo/modelo/excepciones/EstadoTransaccionInvalidoException.java
+++ src/main/java/com/mycompany/sistemabancariodemo/modelo/excepciones/EstadoTransaccionInvalidoException.java
@@ -3,14 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.sistemabancariodemo.modelo.excepciones;
-
+import com.mycompany.sistemabancariodemo.modelo.enums.*;
 /**
  *
  * @author Dell
  */
 public class EstadoTransaccionInvalidoException extends BancoRuntimeException {
     //no nesecita atibutos
-    public EstadoTransaccionInvalidoException(String estadoOrigen,String estadoDestino){
+    public EstadoTransaccionInvalidoException(EstadoTransaccion estadoOrigen,EstadoTransaccion estadoDestino){
         super(
          "Transición inválida: no se puede pasar de " 
             + estadoOrigen + " a " + estadoDestino,
